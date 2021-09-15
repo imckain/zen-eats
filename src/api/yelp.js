55830e30096376f6a608f43db44e614f91d52b41
imckain <ianmmckain@gmail.com>
2021-09-14 20:18:36 -0400

    hiding API key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+const { yelpAPIKey } = YELP_CONFIG;
+
+const YELP_API_KEY = yelpAPIKey;
+
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer oL1yFFRiZLIrsvW3v0ovrxCc3l5byiZalXN1x7qDmuL5EIaL5luWc9rrLrBp5qP-rZ6IekeBe-NYbYCf5nmsKcdwIIzwIBBd60wFvX6vTR_EPIFujxn0f49KAyJBYXYx'
+        Authorization: YELP_API_KEY
     }
 });
