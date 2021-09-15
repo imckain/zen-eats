042691c4b93a5cf2d1a713119d97f57cecc78130
imckain <ianmmckain@gmail.com>
2021-09-15 02:27:09 -0400

    refactored into components for better structure passed props for navigation to detail screen

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+import { FontAwesome } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const ResultsDetail = ({ result }) => {
+
+    return (
+        <View style={styles.containerStyle}>
+            <Image style={styles.imageStyle} source={{ uri: result.image_url }} />
+            <Text style={styles.nameStyle}>{result.name}</Text>
+            <Text>{result.rating}  <FontAwesome name="star" size={14} color="#dbbc54" />  •  {result.review_count} Reviews </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        marginLeft: 15,
+    },
+    imageStyle: {
+        width: 250,
+        height: 150,
+        borderRadius: 4,
+        marginBottom: 5,
+    },
+    nameStyle: {
+        fontSize: 19,
+        fontWeight: 'bold',
+        marginBottom: 3,
+    },
+});
+
+export default ResultsDetail;
