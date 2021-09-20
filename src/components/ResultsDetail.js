1e4b985658cf13d5c8de3741b4e9cb99b0c3c014
imckain <ianmmckain@gmail.com>
2021-09-20 17:41:37 -0400

    clean up

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import { FontAwesome } from '@expo/vector-icons';
 
