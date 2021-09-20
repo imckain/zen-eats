712fb79b92d601d3181b0d7f9af8b7735b61c2b1
imckain <ianmmckain@gmail.com>
2021-09-20 02:58:31 -0400

    added async function for location data calls from device. Location data is being returned properly--  WIP: location useEffect() is making multiple calls for location data, working on fix.

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,18 +1,35 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import yelp from '../api/yelp';
+import * as Location from 'expo-location';
 
 export default () => {
     const [results, setResults] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+            
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
 
     const searchAPI = async (defaultTerm) => {
-        console.log('searched');
         try {
             const response = await yelp.get('/search', {
                 params: {
                     limit: 50,
                     term: defaultTerm,
-                    location: 'columbus'
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                    radius: 4000,
                 }
             });
             setResults(response.data.businesses);
@@ -20,6 +37,9 @@ export default () => {
             setErrorMessage('Something went wrong 😢')
         }
     };
+
+    console.log(`latitude: ${location.coords.latitude}`);
+    console.log(`longitude: ${location.coords.longitude}`);
 
     useEffect(() => {
         searchAPI('')
