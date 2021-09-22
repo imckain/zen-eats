fa44c78c7217d8817ff2e3c907e6cecdc3301c26
imckain <ianmmckain@gmail.com>
2021-09-22 14:08:21 -0400

    uncommented error - code clean up

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,54 +7,58 @@ import ResultsList from '../components/ResultsList';
 
 
 const SearchScreen = props => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [searchAPI, results, errorMessage] = useResults();
-    
-    const filterResultsByPrice = (price) => {
-        return results.filter(result => {
-            return result.price === price;
-        });
-    };
-    
-    return (
-        <View style={styles.resultsContainerStyle}>
-            <SearchBar 
-                searchTerm={searchTerm} 
-                onSearchTermChange={setSearchTerm} 
-                onSearchTermSubmit={() => searchAPI(searchTerm)}
-            />
-            {/* {errorMessage ? <Text>{errorMessage}</Text> : null} */}
-            <ScrollView style={styles.resultsListsStyles}>
-                <ResultsList 
-                    title='Cost Effective' 
-                    results={filterResultsByPrice('$')}
-                    />
-                <ResultsList 
-                    title='Not Cheap' 
-                    results={filterResultsByPrice('$$')}
-                    />
-                <ResultsList 
-                    title='Someone Got Paid' 
-                    results={filterResultsByPrice('$$$')}
-                    />
-                <ResultsList 
-                    title='Fancy Pants' 
-                    results={filterResultsByPrice('$$$$')}
-                />
-                <View style={styles.paddingBlock} />
-            </ScrollView>
-        </View>
-    )
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchAPI, results, errorMessage] = useResults();
+  
+  const filterResultsByPrice = (price) => {
+    return results.filter(result => {
+      return result.price === price;
+    });
+  };
+  
+  return (
+    <View style={styles.resultsContainerStyle}>
+      <SearchBar 
+        searchTerm={searchTerm} 
+        onSearchTermChange={setSearchTerm} 
+        onSearchTermSubmit={() => searchAPI(searchTerm)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <ScrollView style={styles.resultsListsStyles}>
+        <View style={styles.topPaddingBlock} />
+        <ResultsList 
+          title='Cost Effective' 
+          results={filterResultsByPrice('$')}
+          />
+        <ResultsList 
+          title='Not Cheap' 
+          results={filterResultsByPrice('$$')}
+          />
+        <ResultsList 
+          title='Someone Got Paid' 
+          results={filterResultsByPrice('$$$')}
+          />
+        <ResultsList 
+          title='Fancy Pants' 
+          results={filterResultsByPrice('$$$$')}
+        />
+        <View style={styles.bottomPaddingBlock} />
+      </ScrollView>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
-    resultsContainerStyle: {
-        backgroundColor: '#ffffff',
-        flex: 1,
-    },
-    paddingBlock: {
-        height: 40
-    },
+  resultsContainerStyle: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+  },
+  topPaddingBlock: {
+    height: 20
+  },
+  bottomPaddingBlock: {
+    height: 40
+  },
 });
 
 export default SearchScreen;
